926c89bae3aa10a0cf917469fd3cb23f3a57c33b
milkaisgoody <126080236+milkaisgoody@users.noreply.github.com>
2025-09-09 11:09:53 +0900

    백준 11726번

---
+++ dynamic/day26/Practice87.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Practice87 {
+//	static long mod = 10007;
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		long D[] = new long[1001];
+		
+		D[1] = 1;
+		D[2] = 2;
+		
+		for(int i = 3; i <= N;i++) {
+			D[i] = (D[i - 1] + D[i - 2]) % 10007;
+		}
+		
+		System.out.println(D[N]);
+		
+		sc.close();
+	}
+
+}

b2a93862db4b6916881d562fb51c4370262618d2
milkaisgoody <126080236+milkaisgoody@users.noreply.github.com>
2025-10-13 11:20:14 +0900

    백준 24262

---
+++ baekjoon/time_complexity/Baekjoon_24262.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Baekjoon_24262 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		System.out.println("1");
+		System.out.println("0");
+		
+		sc.close();
+	}
+
+}

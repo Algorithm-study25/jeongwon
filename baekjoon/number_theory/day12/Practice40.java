bfecbd39d47767befb4e9810c4ecb693abad4d86
milkaisgoody <126080236+milkaisgoody@users.noreply.github.com>
2026-02-11 11:25:03 +0900

    2회차 : 1016번

---
+++ baekjoon/number_theory/day12/Practice40.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Practice40 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		long min = sc.nextLong();
+		long max = sc.nextLong();
+		
+		boolean[]Check = new boolean[(int) (max - min + 1)];
+		
+		
+		sc.close();
+	}
+
+}
